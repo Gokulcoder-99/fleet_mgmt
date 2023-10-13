1bcc4a7bab3237fbdaa6d64db18e982249428d4a
Gokul27 <gokulkrishnan072@gmail.com>
2023-10-13 11:31:22 +0530

    test commit2

---
+++ fleet_mgmt/test_module/doctype/accident_details/test_accident_details.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Gokul and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAccident_Details(FrappeTestCase):
+	pass
---
+++ fleet_mgmt/test_module/doctype/details_of_driver/details_of_driver.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Gokul and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Details_Of_Driver', {
+	// refresh: function(frm) {
+
+	// }
+});
